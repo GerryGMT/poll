daf668894dfdab01b80c50548942864ec778f731
Gerry <gerry@gmt.gmt>
2021-10-22 20:51:14 +0300

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,73 @@
 <template>
+  <div id="app">
+    <v-card>
+      
+    </v-card>
 
     <v-card>
-      <v-card-title>Select Team</v-card-title>
+      <v-text-field
+        label="Email"
+        :rules="rules"
+      ></v-text-field>
+
+      <v-card-title>Customer Experience</v-card-title>
       <v-divider></v-divider>
       <v-card-text>
         <v-radio-group
-          v-model="radiogroup"
+          v-model="customer"
           column
         >
 
           <v-radio
-            label="ManU"
-            value="manu"
+            label="John"
+            value="john"
             color="red"
           ></v-radio>
 
           <v-radio
-            label="Chelsea"
-            value="chelsea"
+            label="James"
+            value="james"
             color="blue"
           ></v-radio>
 
           <v-radio
-            label="Arsenal"
-            value="arsenal"
+            label="Kim"
+            value="kim"
             color="white"
           ></v-radio>
 
         </v-radio-group>
       </v-card-text>
+      
+      <v-card-title>Wild Card</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-radio-group
+          v-model="wildcard"
+          column
+        >
+
+          <v-radio
+            label="Mary"
+            value="mary"
+            color="red"
+          ></v-radio>
+
+          <v-radio
+            label="Queen"
+            value="queen"
+            color="blue"
+          ></v-radio>
+
+          <v-radio
+            label="Aisha"
+            value="aisha"
+            color="white"
+          ></v-radio>
+
+        </v-radio-group>
+      </v-card-text>
+
       <v-divider></v-divider>
       <v-card-actions>
         <v-btn
@@ -36,18 +75,20 @@
           class="button"
           text
         >
-          Vote
+          Submit
         </v-btn>
       </v-card-actions>
     </v-card>
-
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      radiogroup: null
+      customer: null,
+      wildcard: null,
+      rules: [value => !!value || 'Required.']
     }
   }
 }
